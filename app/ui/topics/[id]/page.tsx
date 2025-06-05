29e41fe222fd1ac4bd9f112ce67e2c6ff5d8b2b7
jaylenperez <jaylenestella24@gmail.com>
2025-06-04 23:34:27 -0500

    fixed formatting

--- app/ui/topics/[id]/page.tsx
+++ app/ui/topics/[id]/page.tsx
@@ -1,11 +1,9 @@
-import React from "react";
-
-interface TopicPageProps {
-  params: {
-    id: string;
-  };
-}
-
-export default async function Page({ params }: TopicPageProps) {
-  return <div>Topics Page: {params.id}</div>;
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const { id } = await params;
+  await new Promise((r) => setTimeout(r, 3000));
+  return <div>Topics Page: {id}</div>;
 }
