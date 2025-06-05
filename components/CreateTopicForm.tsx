cb700e3e40304e77ce1545201134c96336dced4d
jaylenperez <jaylenestella24@gmail.com>
2025-06-04 20:31:10 -0500

    updated a tags to be replaced by Link components task 1 complete :)

--- components/CreateTopicForm.tsx
+++ components/CreateTopicForm.tsx
@@ -1,4 +1,6 @@
+import Link from "next/link";
 export default function CreateTopicForm() {
+
   return (
     <form>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
@@ -20,12 +22,12 @@ export default function CreateTopicForm() {
         </div>
       </div>
       <div className="mt-6 flex justify-end gap-4">
-        <a
+        <Link
           href="/ui"
           className="flex h-10 items-center rounded-lg bg-gray-100 px-4 text-sm font-medium text-gray-600 transition-colors hover:bg-gray-200"
         >
           Cancel
-        </a>
+        </Link>
         <button type="submit">Create Topic</button>
       </div>
     </form>
